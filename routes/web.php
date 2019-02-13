3fb671f454f3e124cacad7e1f51e755b3645e489
C.  Keijzer <c.keijzer@live.nl>
2019-02-13 09:14:13 +0100

    commented 2 routes, because it was not working

--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,12 @@ Route::resource('products', 'ProductController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-<<<<<<< HEAD
 
 /* this is needed for changes in the user edit.blade */
-Route::get('users/{user}',  ['as' => 'users.edit', 'uses' => 'UserController@edit']);
-Route::patch('users/{user}/update',  ['as' => 'users.update', 'uses' => 'UserController@update']);
+// Route::get('users/{user}',  ['as' => 'users.edit', 'uses' => 'UserController@edit']);
+// Route::patch('users/{user}/update',  ['as' => 'users.update', 'uses' => 'UserController@update']);
 
 
-=======
 Route::resource('cart', 'CartController');
 //Route::view('/cart', 'cart');
 //Route::view('/checkout', 'checkout');
->>>>>>> 486c9f2af27bf227507a6ae099a17b8e7cb4b31a
