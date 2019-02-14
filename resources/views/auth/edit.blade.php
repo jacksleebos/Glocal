23344806b8b72a9ff485bfcad20f561ee80d26ab
CodeGorilla <you@example.com>
2019-02-14 13:18:22 +0100

    aanvulling wijzigen profile

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -1,16 +1,25 @@
-<form method="post" action="{{route('users.edit', $user)}}">
-    {{ csrf_field() }}
-    {{ method_field('patch') }}
 
-    <input type="text" name="name"  value="{{ $user->name }}" />
 
-    <input type="text" name="name"  value="{{ $user->name }}" />
+@extends('layout')
 
-    <input type="email" name="email"  value="{{ $user->email }}" />
+@section('content')
 
-    <input type="password" name="password" />
 
-    <input type="password" name="password_confirmation" />
+
+<form method="POST" action="{{ route ('auth.update', $user->id) }}">
+        @method('PATCH')
+        @csrf
+    <input type="text" name="name"  value="{{ $user->name }}" >
+
+    <input type="text" name="address"  value="{{ $user->address }}" >
+
+    <input type="email" name="email"  value="{{ $user->email }}" >
+
+    <input type="password" name="password" >
+
+    <input type="password" name="password_confirmation" >
 
     <button type="submit">Send</button>
 </form>
+
+@endsection
